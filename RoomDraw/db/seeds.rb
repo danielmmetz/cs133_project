9041445381580735d671945a88535c1712e52429
Eric Campbell <1234goal@gmail.com>
2015-10-30 17:14:34 -0700

    Seed database with Users

--- RoomDraw/db/seeds.rb
+++ RoomDraw/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# want 8 char student ids; for simplicty 10000001 10000002
+# for simplicity, lets make passwords just pass1, pass2, pass3
+
+students = %w(10000001 10000002 10000003)
+passwords = %w(pass1 pass2 pass3)
+combination = students.zip passwords
+
+combination.each do |student_id, password|
+  User.create(
+    student_id: student_id,
+    password: password,
+    password_confirmation: password
+  )
+end
+
