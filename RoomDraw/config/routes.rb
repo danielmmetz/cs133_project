211c51f58d559c0cb2fdc9a39dc486568b9e0d0a
Eric Campbell <1234goal@gmail.com>
2015-11-02 20:11:37 -0800

    Set Landing page

--- RoomDraw/config/routes.rb
+++ RoomDraw/config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get '/users_login', to: 'users#show', as: 'user'
-
-  resources :users
-
-  # get 'session/new'
+  root 'session#new'
 
   get    'login' => 'session#new'
   post   'login' => 'session#create'
   delete 'login' => 'session#destroy'
+
+  # log the user in.
+  get '/users_login', to: 'users#show', as: 'user'
+
+  resources :users
+  get 'users/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
