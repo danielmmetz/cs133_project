1cbc2ccbded16942a82de3ba2b7cc5a9de3d9e07
Eric Campbell <1234goal@gmail.com>
2015-11-09 10:50:54 -0800

    display members in group

--- RoomDraw/app/helpers/groups_helper.rb
+++ RoomDraw/app/helpers/groups_helper.rb
@@ -4,4 +4,20 @@ module GroupsHelper
   def draw_groups
     DrawGroup.where student_id: current_user.student_id
   end
+
+  def students_in(draw_group)
+    Student.find_by_sql students_query draw_group
+  end
+
+  private
+
+  # a query that gets the students in a particular draw_group
+  def students_query(draw_group)
+    "
+    SELECT students.*
+    FROM members, students
+    WHERE members.student_id = students.id
+      AND members.draw_group_id = #{draw_group.id}
+    "
+  end
 end
