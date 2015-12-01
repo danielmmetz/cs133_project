76b02fde953c5e0e71a1235c451ca20e04efbd43
Eric Campbell <1234goal@gmail.com>
2015-12-01 14:21:47 -0800

    Fix Occupy model

--- RoomDraw/app/models/occupy.rb
+++ RoomDraw/app/models/occupy.rb
@@ -4,14 +4,15 @@ class Occupy < ActiveRecord::Base
   self.primary_key = [:student_id, :dorm_name, :room_num]
 
   # define validations
-  validate :fall_or_spring?
-  validate :year, presence: true
+  validate { fall_or_spring? }
+  validates :year, presence: true
 
   private
 
   def fall_or_spring?
     unless in_fall? or in_spring?
       errors.add :base, "Student must occupy a room at least one semester a year"
+    end
   end
 
 end
