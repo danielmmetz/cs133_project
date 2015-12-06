7a6908397acee9853c964b8f9e95c49874c9dea8
Daniel Metz <danielmmetz@gmail.com>
2015-12-05 16:40:21 -0800

    display collections instead of rooms

---
+++ RoomDraw/app/services/collection_display_service.rb
@@ -0,0 +1,17 @@
+class CollectionDisplayService
+
+  def initialize
+  end
+
+  def retrieve(col_id)
+    r = Room.where collection_id: col_id
+    {id: col_id,
+    rooms: r,
+    capacity: (r.sum :capacity),
+    dorm_name: r.first[:dorm_name],
+    num_rooms: r.size,
+    room_nums: (r.pluck :room_num).sort,
+    room_capacities: (r.pluck :capacity).sort}
+  end
+
+end
