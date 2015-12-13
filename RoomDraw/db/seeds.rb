9a5739ca5420161c7caeed14f3167aae0da5fea2
Daniel Metz <danielmmetz@gmail.com>
2015-12-12 16:31:50 -0800

    added seed string for determinism

--- RoomDraw/db/seeds.rb
+++ RoomDraw/db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'set'
+srand(47) # set a seed!
 
 ###############################################################################################################
 # create students
